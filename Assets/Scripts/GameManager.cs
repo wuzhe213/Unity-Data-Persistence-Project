89d16811c13dd1fb04dcfadb78ee42c0a56caa93
wuzhe213 <wuzhe213@163.com>
2021-06-27 01:16:23 +0800

    menu scene ready

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    public Text bestScoreText;
+    public InputField nameField;
+    public Button startButton;
+    public Button quitButton;
+    public Text errorText;
+
+    public BestScore bestScore;
+
+    public static GameManager Instance;
+
+    private string name;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameField.onEndEdit.AddListener(ChangeName);
+        startButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ChangeName(string name)
+    {
+        this.name = name;
+    }
+
+    private void StartGame()
+    {
+        if (nameField.text == "" || nameField.text == null)
+        {
+            errorText.gameObject.SetActive(true);
+        }
+        else
+        {
+            errorText.gameObject.SetActive(false);
+
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();    
+#endif
+    }
+
+    public void SetBestScore(int score)
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.bestScore.score = score;
+        }
+    }
+
+    public void SetBestName(int score)
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.bestScore.name = name;
+        }
+    }
+}
+
+[Serializable]
+public class BestScore
+{
+    public string name;
+    public int score;
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,21 +11,22 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text bestScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
